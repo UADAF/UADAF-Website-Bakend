5e3709785e7f75c0a37d376bed85dd56e843af79
TheEvilRoot <robot-creatix@ya.ru>
2019-06-10 14:49:01 +0300

    Update QuoterV2Api.kt
    Refactored QV2API as part of simulate requests feature

--- src/main/kotlin/api/QuoterV2Api.kt
+++ src/main/kotlin/api/QuoterV2Api.kt
@@ -107,7 +107,7 @@ object QuoterV2Api {
         } != 0
     }
 
-    suspend fun <T: Any> PipelineContext<Unit, ApplicationCall>.handle(func: () -> T, check: Boolean = true ,respond: (T) -> Any = { it }) {
+    suspend fun <T: Any> PipelineContext<Unit, ApplicationCall>.handle(func: () -> T, check: Boolean = true, respond: (T) -> Any = { it }) {
         try {
             val result = func()
             if (check && result is List<*> && result.isEmpty()) {
@@ -115,6 +115,140 @@ object QuoterV2Api {
             }
 
             call.respond(respond(result))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            call.respond(InternalServerError)
+        }
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.requestAdd(payloadFunction: (String, String, String, String, List<String>) -> Unit) {
+        val key = call.request.header("Access-Key")
+                ?: return call.respond(Forbidden)
+
+        if (!Core.verifyKey(key)) {
+            return call.respond(Unauthorized)
+        }
+
+        val params = call.receiveParameters()
+
+        val adder = params["adder"]
+                ?: return call.respond(BadRequest)
+        val authors = params["authors"]
+                ?: return call.respond(BadRequest)
+        val displayType = params["dtype"]
+                ?: "text"
+        val content = params["content"]
+                ?: return call.respond(BadRequest)
+        val attachments = params["attachments"]?.split(";") ?:
+        emptyList()
+
+        if (displayType !in setOf("text", "dialog"))
+            return call.respond(BadRequest)
+
+        try {
+            payloadFunction(adder, authors, content, displayType, attachments)
+            call.respond(OK)
+        } catch(e: SQLException) {
+            call.respond(InternalServerError)
+        }
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.requestAttach(payloadFunction: (Int, String) -> AttachmentResult) {
+        val key = call.request.header("Access-Key")
+                ?: return call.respond(Forbidden)
+
+        if (!Core.verifyKey(key)) {
+            return call.respond(Unauthorized)
+        }
+
+        val params = call.receiveParameters()
+        val id = params["id"]?.toIntOrNull()
+                ?: return call.respond(BadRequest)
+        val attachment = params["attachment"]
+                ?: return call.respond(BadRequest)
+
+        val quoteExists = isExists(id)
+        val attachmentExists = AttachmentsApi.isExists(attachment)
+
+        if (!quoteExists) return call.respond(NotFound)
+        if (!attachmentExists) return call.respond(Gone)
+
+        return try {
+            when (payloadFunction(id, attachment)) {
+                QuoterV2Api.AttachmentResult.Attached           -> call.respond(OK)
+                QuoterV2Api.AttachmentResult.AlreadyAttached    -> call.respond(Conflict)
+                QuoterV2Api.AttachmentResult.Error              -> call.respond(NotFound)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            call.respond(InternalServerError)
+        }
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.requestGet(payloadFunction: (Int) -> List<QuoteV2>) {
+        val id = call.parameters["id"]?.toIntOrNull()
+                ?: return call.respond(BadRequest)
+
+        handle({ payloadFunction(id) }) { it.first() }
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.requestFromTo(payloadFunction: (Int, Int) -> List<QuoteV2>) {
+        val from = call.parameters["from"]?.toIntOrNull()
+                ?: return call.respond(BadRequest)
+        val to = call.parameters["to"]?.toIntOrNull()
+                ?: return call.respond(BadRequest)
+        if (from > to) return call.respond(BadRequest)
+
+        handle({ payloadFunction(from, to) }, check=false)
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.requestRandom(payloadFunction: (Int) -> List<QuoteV2>) {
+        val count = (call.parameters["count"] ?: "1").toIntOrNull() ?:
+        return call.respond(BadRequest)
+        if (count < 0) return call.respond(BadRequest)
+
+        handle({ payloadFunction(count) }, check=false)
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.proceedEdit(payloadFunction: (Int, String, Long, String) -> Boolean) {
+        val key = call.request.header("Access-Key")
+                ?: return call.respond(Forbidden)
+
+        if (!Core.verifyKey(key)) {
+            return call.respond(Unauthorized)
+        }
+
+        val params = call.receiveParameters()
+        val id = params["id"]?.toIntOrNull()
+                ?: return call.respond(BadRequest)
+        val editedBy = params["edited_by"]
+                ?: return call.respond(BadRequest)
+        val newContent = params["new_content"]
+                ?: return call.respond(BadRequest)
+
+        try {
+            return if (payloadFunction(id, editedBy, System.currentTimeMillis(), newContent)) {
+                call.respond(OK)
+            } else {
+                call.respond(NotFound)
+            }
+        } catch(e: Exception) {
+            e.printStackTrace()
+            call.respond(InternalServerError)
+        }
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.proceedFixIds(payloadFunction: () -> Unit) {
+        val key = call.request.header("Access-Key")
+                ?: return call.respond(Forbidden)
+
+        if (!Core.verifyKey(key)) {
+            return call.respond(Unauthorized)
+        }
+
+        try {
+            payloadFunction()
+            return call.respond(OK)
         } catch (e: Exception) {
             e.printStackTrace()
             call.respond(InternalServerError)
@@ -141,37 +275,7 @@ object QuoterV2Api {
          *  * OK - added
          *  * ISE - exception
          */
-        put("/") {
-            val key = call.request.header("Access-Key")
-                    ?: return@put call.respond(Forbidden)
-
-            if (!Core.verifyKey(key)) {
-                return@put call.respond(Unauthorized)
-            }
-
-            val params = call.receiveParameters()
-
-            val adder = params["adder"]
-                    ?: return@put call.respond(BadRequest)
-            val authors = params["authors"]
-                    ?: return@put call.respond(BadRequest)
-            val displayType = params["dtype"]
-                    ?: "text"
-            val content = params["content"]
-                    ?: return@put call.respond(BadRequest)
-            val attachments = params["attachments"]?.split(";") ?:
-                    emptyList()
-
-            if (displayType !in setOf("text", "dialog"))
-                return@put call.respond(BadRequest)
-
-             try {
-                addQuote(adder, authors, content, displayType, attachments)
-                call.respond(OK)
-            } catch(e: SQLException) {
-                call.respond(InternalServerError)
-            }
-        }
+        put("/") { requestAdd(::addQuote) }
 
         /**
         * Forbidden - if access key not passed
@@ -183,37 +287,7 @@ object QuoterV2Api {
         * Conflict - already attached
         * ISE - exception
         */
-        put("attach") {
-            val key = call.request.header("Access-Key")
-                    ?: return@put call.respond(Forbidden)
-
-            if (!Core.verifyKey(key)) {
-                return@put call.respond(Unauthorized)
-            }
-
-            val params = call.receiveParameters()
-            val id = params["id"]?.toIntOrNull()
-                    ?: return@put call.respond(BadRequest)
-            val attachment = params["attachment"]
-                    ?: return@put call.respond(BadRequest)
-
-            val quoteExists = isExists(id)
-            val attachmentExists = AttachmentsApi.isExists(attachment)
-
-            if (!quoteExists) return@put call.respond(NotFound)
-            if (!attachmentExists) return@put call.respond(Gone)
-
-            try {
-                return@put when (addAttachment(id, attachment)) {
-                    QuoterV2Api.AttachmentResult.Attached           -> call.respond(OK)
-                    QuoterV2Api.AttachmentResult.AlreadyAttached    -> call.respond(Conflict)
-                    QuoterV2Api.AttachmentResult.Error              -> call.respond(NotFound)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                return@put call.respond(InternalServerError)
-            }
-        }
+        put("attach") { requestAttach(::addAttachment) }
 
         /**
          * BadRequest - id not passed or invalid
@@ -221,40 +295,21 @@ object QuoterV2Api {
          * OK - succeed
          * ISE - exception
          */
-        get("{id}") {
-            val id = call.parameters["id"]?.toIntOrNull()
-                    ?: return@get call.respond(BadRequest)
-
-            handle({ getById(id) }) { it.first() }
-        }
+        get("{id}") { requestGet(::getById) }
 
         /**
          * BadRequest - if params not passed or invalid
          * OK - succeed
          * ISE - exception
          */
-        get("{from}/{to}") {
-            val from = call.parameters["from"]?.toIntOrNull()
-                    ?: return@get call.respond(BadRequest)
-            val to = call.parameters["to"]?.toIntOrNull()
-                    ?: return@get call.respond(BadRequest)
-            if (from > to) return@get call.respond(BadRequest)
-
-            handle({ getRange(from, to) }, check=false)
-        }
+        get("{from}/{to}") { requestFromTo(::getRange) }
 
         /**
          * BadRequest - if params not passed or invalid
          * OK - succeed
          * ISE - exception
          */
-        get("random/{count?}") {
-            val count = (call.parameters["count"] ?: "1").toIntOrNull() ?:
-                    return@get call.respond(BadRequest)
-            if (count < 0) return@get call.respond(BadRequest)
-
-            handle({ getRandom(count) }, check=false)
-        }
+        get("random/{count?}") { requestRandom(::getRandom) }
 
         /**
          * OK - succeed
@@ -275,50 +330,9 @@ object QuoterV2Api {
          * OK - succeed
          * ISE - exception
          */
-        post("edit") {
-            val key = call.request.header("Access-Key")
-                    ?: return@post call.respond(Forbidden)
+        post("edit") { proceedEdit(::editQuote) }
 
-            if (!Core.verifyKey(key)) {
-                return@post call.respond(Unauthorized)
-            }
-
-            val params = call.receiveParameters()
-            val id = params["id"]?.toIntOrNull()
-                    ?: return@post call.respond(BadRequest)
-            val editedBy = params["edited_by"]
-                    ?: return@post call.respond(BadRequest)
-            val newContent = params["new_content"]
-                    ?: return@post call.respond(BadRequest)
-
-            try {
-                if (editQuote(id, editedBy, System.currentTimeMillis(), newContent)) {
-                    return@post call.respond(OK)
-                } else {
-                    return@post call.respond(NotFound)
-                }
-            } catch(e: Exception) {
-                e.printStackTrace()
-                call.respond(InternalServerError)
-            }
-        }
-
-        post("fix_ids") {
-            val key = call.request.header("Access-Key")
-                    ?: return@post call.respond(Forbidden)
-
-            if (!Core.verifyKey(key)) {
-                return@post call.respond(Unauthorized)
-            }
-
-            try {
-                fixIds()
-                return@post call.respond(OK)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                call.respond(InternalServerError)
-            }
-        }
+        post("fix_ids") { proceedFixIds(::fixIds) }
     }
 
 }
